2350b3c4517a73c3f2ec06a8baddc5924ca0ae3a
Sandwell <nicogonzasandoval@hotmail.com>
2023-10-25 12:36:13 -0300

    Instalacion react icons, carpetas ItemListContainer y Count creadas

--- entrega1/src/App.jsx
+++ entrega1/src/App.jsx
@@ -2,14 +2,18 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Navbar from './components/Navbar/Navbar';
+import Navbar from './components/Navbar/Navbar'
+import ItemListContainer from './components/ItemListContainer/ItemListContainer'
 
 function App() {
   const [count, setCount] = useState(0)
+  const saludo= "Bienvenidos a mi primer E-Commerce en React"
 
   return (
     <>
       <Navbar/>
+      <ItemListContainer saludo={saludo}/>
+
     </>
   )
 }
